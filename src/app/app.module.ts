af6bea584e4c39f8e56935b38e6445a2a4276a01
Chansreynich Huot <huot.nich8888@gmail.com>
2022-08-16 11:25:15 +0700

    reactive form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TemplateFormComponent } from './template-form/template-form.component';
-import {FormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 
 @NgModule({
@@ -14,7 +14,8 @@ import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-reactive-form',
@@ -6,10 +7,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reactive-form.component.css']
 })
 export class ReactiveFormComponent implements OnInit {
+// title = new FormControl('');
 
-  constructor() { }
+// formArticle = new FormGroup({
+//   title : new FormControl(),
+//   author: new FormControl(),
+//   description: new FormControl()
+// })
+
+
+  formArticle = this.formBuilder.group({
+    title: ['',Validators.required,],
+    author:[''],
+    description:[''],
+
+  })
+  constructor(private formBuilder: FormBuilder) { }
+
+  onSubmit(){
+  // this.formArticle.setValue({
+  //   title:'Updated title',
+  //   author:'abc',
+  //   description: 'description'
+  // });
+  //   this.formArticle.patchValue({
+  //     title:'Updated title',
+  //     // author:'abc',
+  //     // description: 'description'
+  //   });
+    // console.log(this.formArticle);
+  }
+  updateTitle(){
+    // this.title.setValue('SingChheng');
+  }
 
   ngOnInit(): void {
+
   }
 
 }
